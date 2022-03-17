bdd2236e1fccef1f868eaedf7601eb030f1a5b9d
nbthuan <thu2154992@maricopa.edu>
2022-03-16 17:06:53 -0700

    Project: It's in the Bag Name: Thuan Nguyen

---
+++ src/com/example/iterable/Bag.java
@@ -0,0 +1,40 @@
+package com.example.iterable;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Bag<T> implements Container<T> {
+    ArrayList<T> arrL = new ArrayList<T>();
+
+    @Override
+    public boolean isEmpty() {
+        return arrL.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return arrL.size();
+    }
+
+    @Override
+    public void add(T item) {
+        arrL.add(item);
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return arrL.iterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+        Container.super.forEach(action);
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return Container.super.spliterator();
+    }
+}
